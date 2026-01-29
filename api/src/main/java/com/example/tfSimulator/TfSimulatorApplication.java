bce26ef27679c51490a7f88467feb91e5178e81a
pkstoy13 <pkstoyanov1@gmail.com>
2026-01-29 17:43:05 -0500

    setting up backend

---
+++ api/src/main/java/com/example/tfSimulator/TfSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tfSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfSimulatorApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/tfSimulator/TfSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tfSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
