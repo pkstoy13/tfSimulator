9210d5b2b01dcca7932c97aff7537ac523e3f346
pkstoy13 <pkstoyanov1@gmail.com>
2026-01-29 18:08:21 -0500

    working on setting up Docker container, should verify the server locally first

--- api/src/main/java/com/example/tfSimulator/TfSimulatorApplication.java
+++ api/src/main/java/com/example/tfSimulator/TfSimulatorApplication.java
@@ -8,6 +8,9 @@ public class TfSimulatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TfSimulatorApplication.class, args);
+		System.out.println("Backend Running!");
+
+
 	}
 
 }
